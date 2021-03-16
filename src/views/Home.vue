6f6e99e4f7997dcb376cf44784fca536fbfdcd74
clartor <clara.tornblom@gmail.com>
2021-03-16 16:01:28 +0100

    hej

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home">
     <img src="../../public/Keramik/kannaTre.png" alt="">
-    <img src="../../public/Keramik/KannaEn.png" alt="">
+    <img src="../../public/Keramik/kannaEn.png" alt="">
     <img src="../../public/Keramik/kopparRibb.png" alt="">
-    <img src="../../public/Keramik/kommarSläta.png" alt="">
+    <img src="../../public/Keramik/kopparSläta.png" alt="">
     <img src="../../public/Keramik/kopparSmåÖron.png" alt="">
     <img src="../../public/Keramik/Mortlar.png" alt="">
     <img src="../../public/Keramik/Servett.png" alt="">
@@ -16,3 +16,10 @@ export default {
   
 }
 </script>
+<style>
+div{
+  display: grid;
+
+}
+
+</style>
