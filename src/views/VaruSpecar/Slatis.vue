421fe71dddfb1a58bdd28ffd580f33adda07421c
clartor <clara.tornblom@gmail.com>
2021-03-18 17:39:31 +0100

    bu

--- src/views/VaruSpecar/Slatis.vue
+++ src/views/VaruSpecar/Slatis.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="slatis">
         <h1>Slätis</h1>
+        <!-- <img src="../../public/keramikBilder/kopparSläta.jpg" alt="" class="produkt2"/> -->
+        <p>Slätis</p>
     </div>
 </template>
