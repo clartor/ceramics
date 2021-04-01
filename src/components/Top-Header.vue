23828c97d1d62f9499dc9a3ce1896c8a45de32f0
moalinden <moa.linden1@gmail.com>
2021-04-01 18:19:35 +0200

    loginstufffix

---
+++ src/components/Top-Header.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <br>  <!--Logged In-->
+        <span v-if="loggedIn"><button @click="signOut">Sign Out</button></span>    <!-- Sign Out Button - Only Visible if logged in -->
+        <span v-else></span>  <!--No-->
+        <div>                      <!--  v-if="firebase.auth().currentUser"   -->
+            <!--<button @click="signOut">Sign Out</button>-->
+        </div>
+    </div>
+</template>
+
+<script>
+import firebase from "firebase/app";
+import "firebase/auth";
+export default {
+    created() {                       /* Sign Out Logic */
+        firebase.auth().onAuthStateChanged(user=> {
+            this.loggedIn = !!user;
+            // if(user){
+            //     this.loggedIn = true;
+            // } else {
+            //     this.loggedIn = false;
+            // }
+        })
+    },
+    data() {
+        return {
+            loggedIn: false
+        }
+    },
+    methods: {
+        async signOut() {             /* Sign Out Logic */
+            try {
+                const data = await firebase.auth().signOut();
+                console.log(data);
+                this.$router.replace({name: "Login"})
+            } catch (err) {
+                console.log(err)
+            }
+
+        }
+    }
+
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,10 +2,9 @@
   <div class="register">
     <link rel="stylesheet" href="../../style.css" />
     <h1>Register</h1>
-    <form @submit.prevent="Register">
+    <form @submit.prevent="pressed">
       <input type="text" placeholder="Email" v-model="email"/>
       <input type="password" placeholder="Password" v-model="password"/>
-      <!-- <input type="password" placeholder="Repeat Password"/> must match -->
       <input type="submit" value="Register" />
       <p class="loginLink">Redan medlem? <router-link to="/login">Logga in här</router-link></p>
     </form>
@@ -13,35 +12,34 @@
   </div>
 </template>
 
-<script>
-import firebase from "firebase";
-import { ref } from "vue";
-
-export default {
-  setup() {
-    const email = ref("");
-    const password = ref("");
-
-    const Register = () => {
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(email.value, password.value)
-        .then((data) => console.log(data))
-        .catch((err) => alert(err.message));
-    };
-    //     .then((user) => {
-    //       alert(user);
-    //     })
-    //     .catch((err) => alert(err.message));
-    // };
-    return {
-      Register,
-      email,
-      password,
-    };
-  },
-};
-</script>
 
 <style>
 </style>
+
+<script>
+import firebase from "firebase/app";
+import "firebase/auth";
+export default {
+    methods: {
+        async pressed() {
+            try {
+
+            const user = firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+            console.log(user)
+            this.$router.replace({name: "Kassa"});
+            } catch (err) {
+                console.log(err)
+            }
+
+        }
+    },
+    data() {
+        return {
+            email: "",
+            password: '',
+            error: ''
+        }
+    }
+
+}
+</script>
